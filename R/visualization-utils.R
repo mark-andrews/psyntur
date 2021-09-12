5532c0cc8f5215cb92ccd26c81d75d9fcc9e8dcf
Mark Andrews <mjandrews.org@gmail.com>
2021-09-12 14:47:50 +0100

    Add interaction_line_plot
    Closes #18

--- R/visualization-utils.R
+++ R/visualization-utils.R
@@ -393,3 +393,38 @@ scatterplot_matrix <- function(.data, ..., .by = NULL, .bins = 10){
     ) + ggplot2::theme_minimal()
   
 }
+
+
+#' Make a interaction line plot
+#'
+#' @param x A continuous variable to be plotted along the x-axis
+#' @param y A continuous variable to be plotted along the y-axis
+#' @param by A categorical variable by which we split the data and create one line plot for each resulting group
+#' @param data A data frame with the `x`, `y`, `by` variables
+#' @param ylim A vector of limits for the y-axis
+#'
+#' @return A ggplot2 object
+#' @export
+#'
+#' @examples
+#' interaction_line_plot(y = score, x = time, by = treatment, data = selfesteem2_long, ylim = c(70, 100))
+interaction_line_plot <- function(y, x, by, data, ylim = NULL){
+  
+  Df <- group_by(data, {{x}}, {{by}}) %>% 
+    summarise(mean = mean({{y}}),
+              sem = sd({{y}})/sqrt(length({{y}})),
+              .groups = 'drop')
+  
+  the_aes <- aes(x = {{ x }}, y = mean, group = {{ by }}, colour = {{ by }})
+  
+  p1 <- ggplot(Df, mapping = the_aes) + geom_point() + geom_line()
+  
+  p1 <- p1 + geom_linerange(aes(x = {{ x }}, ymin = mean - sem, ymax = mean + sem), size = .3)
+  
+  if (!is.null(ylim)){
+    p1 <- p1 + scale_y_continuous(limits = ylim)
+  }
+  
+  p1 + theme_classic() + scale_colour_brewer(palette = "Set1") 
+  
+}
