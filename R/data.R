900b29176b2e49ac40ac9b5d9826a7a7d99421b6
Mark Andrews <mjandrews.org@gmail.com>
2021-09-12 14:55:52 +0100

    Add `selfesteem2` data
    Closes #29

--- R/data.R
+++ R/data.R
@@ -168,3 +168,32 @@ NULL
 #' data(jobsatisfaction)
 #' jobsatisfaction
 NULL
+
+
+#'Self Esteem Score Data for Two-way Repeated Measures ANOVA
+#'
+#'@description Data are the self esteem score of 12 individuals enrolled in 2
+#'  successive short-term trials (4 weeks) - control (placebo) and special diet
+#'  trials.
+#'
+#'  The self esteem score was recorded at three time points: at the beginning
+#'  (t1), midway (t2) and at the end (t3) of the trials.
+#'
+#'  The same 12 participants are enrolled in the two different trials with
+#'  enough time between trials.
+#'
+#'  Two-way repeated measures ANOVA can be performed in order to determine
+#'  whether there is interaction between time and treatment on the self
+#'  esteem score.
+#'  
+#'  This data set was taken from the
+#'   \href{https://cran.r-project.org/package=datarium}{`datarium` R
+#'   package}.
+#'@name selfesteem2
+#'@docType data
+#'@usage data("selfesteem2")
+#'@format A data frame with 24 rows and 5 columns.
+#' @examples
+#' data(selfesteem2)
+#' selfesteem2
+NULL
